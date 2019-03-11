7565862bab2c1cea776d205a7bb547bafb73a05e
ASMAA-86 <asmabaabdullah@hotmail.com>
2019-03-11 13:53:22 +0300

    login as the HomePage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :applies
-  root 'pages#home'
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
   #comment
   # root "main#page"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
