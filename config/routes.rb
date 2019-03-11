64d1a1b0b1da114349863f5da3ea3d2c99a19d7f
ASMAA-86 <asmabaabdullah@hotmail.com>
2019-03-11 14:25:00 +0300

    Right login Home

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
   resources :applies
   devise_scope :user do
-    root to: "devise/sessions#new"
+    
+    authenticated :user do
+      root "pages#home"
+    end
+
+    unauthenticated do
+      root to: "devise/sessions#new"
+    end
   end
+
   #comment
-  # root "main#page"
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
