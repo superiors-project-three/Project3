f2b568cd9575ffcd45ba4eef318be86993fb8e4f
ASMAA-86 <asmabaabdullah@hotmail.com>
2019-03-11 14:25:00 +0300

    Right login Home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    # def after_sign_in_path_for(resource_or_scope)
+    #     redirect_to "devise/sessions#new" and return
+    # end
+
+    # def after_sign_out_path_for(resource_or_scope)
+    #     redirect_to "devise/sessions#new" and return
+    # end
 end
